87e32f91dc9162a7d71d08eadebe28ed92c6432d
Vičar Tomáš (147472) <vicar@vutbr.cz>
2021-03-18 22:40:53 +0100

    ddf

--- config.py
+++ config.py
@@ -69,11 +69,11 @@ class Config:
     
     
     LEVELS = 7
-    LVL1_SIZE = 6*7
+    LVL1_SIZE = 6*6
     OUTPUT_SIZE = len(SNOMED2IDX_MAP)
     CONVS_IN_LAYER = 3
     BLOCKS_IN_LVL = 3
-    FILTER_SIZE = 7
+    FILTER_SIZE = 3
     
     # DO = 0.3
     DO = None
